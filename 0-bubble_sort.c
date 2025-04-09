e8fc15f38f0cb0890cf7878041f853d260d89727
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-04-09 16:25:59 +0200

    create README file, 0-O for answers about algo, 2 functions files and 0-bubble_sort.c file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+* bubble_sort - sorts an array of integers in ascending
+* order using the Bubble sort algorithm
+* @array: the array to be printed
+* @size: the size of the array
+*
+* Return: Nothing. The array will be printed each time
+* after 2 elements have been swapped
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp = 0;
+/*Array traversal - 1 at each iteration because the last element is in place*/
+	for (i = 0; i < size - 1; i++)
+	{
+/*Array traversal until (i - 1) at each iteration*/
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j + 1] < array[j])
+			{
+			/** Swap of 2 elements */
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+			}
+		}
+
+		print_array(array, size);
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is the README for Holberton School Algorithms tasks
